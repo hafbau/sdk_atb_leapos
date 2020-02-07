a12f3975549d7eff987d5b7f5c4f63b47508952d
Hafiz Suara <hafbau@yahoo.com>
2020-02-06 22:58:32 -0700

    implemented login, defaulting it to env vars

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ const sdkFunks = resource_docs.reduce((sdk, doc) => {
     return sdk;
 }, {});
 
+sdkFunks.login = ({ username, password, consumer_key } = {
+   username: process.env.OPB_USERNAME, 
+   password: process.env.OPB_PASSWORD, 
+   consumer_key: process.env.OPB_CONSUMER_KEY
+}) => {
+    return axios({
+        method: 'POST',
+        url: '/my/logins/direct',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `DirectLogin username="${username}",password="${password}",consumer_key="${consumer_key}"`
+        }
+    })
+    .then(({ token }) => {
+        axios
+        .defaults
+        .headers
+        .common['Authorization'] = `DirectLogin token="${token}"`
+        return { token }
+    })
+}
+
 // This algorithm feels somehow; splitting and then joining. Where is String#replace with regex and callback when you need them
 const putParamsInUrl = (url, params = {}) => {
     if (!url.includes('_ID')) return url;
